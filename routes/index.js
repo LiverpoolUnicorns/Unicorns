4978a2f2b51dc5639371e0743492f22b3759c19f
josephyearsley <joggino23@gmail.com>
2015-04-19 15:14:39 +0100

    Just need to do timing to send back, and test

--- routes/index.js
+++ routes/index.js
@@ -101,14 +101,14 @@ router.get('/api/busDriver/:driverCode/:line/:lastStop/:long/:lat', function(req
     /**
      * SORT IN HERE THE ROUTE DOC, SO IF PAST ALL THEN WE HAVE ROUTE FOR THAT LINE!
      */
-    //See that there's no route, so then check to find last stop
+        //See that there's no route, so then check to find last stop
     Routes.find({line: req.param('line')}, function(err,doc){
         //Back to the start, so unravel and save into routes
         if(doc.length == 0){
             //find by last stop, then sort ascending
             Driver.find({lastStop: req.param('lastStop')}).sort(date).exec(function(err,docs){
                 if(docs.length > 1){
-                     temp = [];
+                    temp = [];
 
                     //Go through docs and insert into array. Don't double count the last one.
                     for(i=0;i>docs.length-1;i++){
@@ -146,23 +146,62 @@ router.get('/api/nextBus/:line/:locationCode/:long/:lat', function(req,res){
      * Future can use a neural network to learn timings based on time, day, weather and average speed. (SOM?)
      */
 
-    Routes.find({line: req.param('line')}, function(err,data){
-        if(data.length > 0)
-            Driver.find({line: 'line', code: 'code'}).sort(-date).exec(function(err,docs){
+    Routes.find({line: req.param('line')}, function(err,data) {
+        if (data.length > 0)
+            Driver.aggregate(
+                [
+                    // Grouping pipeline
+                    {
+                        "$group": {
+                            "line": '$line',
+                            "code": '$code'
+                        }
+                    },
+                    // Sorting pipeline
+                    {"$sort": {"$date": -1}},
+                    // Optionally limit results
+                    {"$limit": 5}
+                ],
+                function (err, result) {
+                    var data = [];
+                    // Result is an array of documents
+                    //Remove an documents which don't have correct line code.
+                    //only use latest $dates, then plug into api
+                    result.forEach(function (entry) {
 
-            })
-    })
-    res.sendStatus(200);
-    /*if line in DB
-     can see if in radius then exclude from analysis, by looking up stop code and position in array
-     else
-     agg on driver and line, order on time. if its in the past then its visited your stop.
+                            var request = http.request(
+                                'http://open.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluu82qu12h%2C2x%3Do5-94y2lu&ambiguities=ignore&unit=m&from=' +
+                                entry.lon + ',' + entry.lat + '&to=' + data.lon + ',' + data.lat + '&callback=renderNarrative',
+                                function (response) {
+                                    console.log('STATUS: ' + response.statusCode);
+                                    console.log('HEADERS: ' + JSON.stringify(response.headers));
+                                    response.setEncoding('utf8');
+                                    response.on('data', function (chunk) {
+                                        data.push(chunk);
+                                    });
+                                    response.on('end', function () {
+                                       });
+                                    request.end();
+                                });
+                        }
+                    );
+                    min = data[0].distance;
+                    index = 0;
+                    for(i=0;i>data.length;i++){
+                        if(data[i].distance < min & result[i].line == req.param('line')){
+                            min = data[i];
+                            index = i;
+                        }
+                    }
+                    //DO TIMING AND SEND BACK TO USER
+                    res.send()
+                    });
+                });
 
-     var driver+line = above
 
-     var estimate time = route the locations and take distance, and average speed.
-     return estimate timing, and if delayed or not, as well as lat and long
-     */
+
+        res.sendStatus(200);
+
 });
 
 
